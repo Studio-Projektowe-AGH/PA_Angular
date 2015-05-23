3b1b312985c90ff90e1a55d1b993ec1ebd4992dd
Putis <marek.28.93@gmail.com>
2015-05-23 15:01:47 +0200

    poprawki na testach

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
@@ -17,10 +18,10 @@ public class IntegrationTest {
      */
     @Test
     public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), new HtmlUnitDriver(), new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
+                assertThat(browser.pageSource()).contains("Start simulation");
             }
         });
     }
